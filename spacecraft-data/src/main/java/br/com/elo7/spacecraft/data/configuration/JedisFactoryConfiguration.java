d12c470f63e8262ba1757571ea7e190204a26ee4
Fernando Stoiano Gonzalez <fernando.gonzalez5@walmart.com>
2016-05-11 00:41:26 -0300

    Implemented spring property-placeholder for external files configurations

--- spacecraft-data/src/main/java/br/com/elo7/spacecraft/data/configuration/JedisFactoryConfiguration.java
+++ spacecraft-data/src/main/java/br/com/elo7/spacecraft/data/configuration/JedisFactoryConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.elo7.spacecraft.data.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
@@ -9,15 +10,37 @@ import redis.clients.jedis.JedisPoolConfig;
 @Configuration
 public class JedisFactoryConfiguration {
 	
+	@Value("${spacecraft.redis.host}")
+	private String redisHost;
+	
+	@Value("${spacecraft.redis.port}")
+	private int redisPort;
+	
+	@Value("${spacecraft.redis.use.pool}")
+	private boolean usePool;
+	
+	@Value("${spacecraft.redis.timeout}")
+	private Long redisTimeout;
+	
+	@Value("${spacecraft.redis.MaxTotal}")
+	private int redisMaxTotal;
+	
+	@Value("${spacecraft.redis.MinIdle}")
+	private int redisMinIdle;
+	
+	@Value("${spacecraft.redis.MaxIdle}")
+	private int redisMaxIdle;
+	
+	
 	@Bean
 	public JedisConnectionFactory jedisConnectionFactory() {
 		
 		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-		jedisConnectionFactory.setHostName("localhost");
-		jedisConnectionFactory.setPort(6379);
+		jedisConnectionFactory.setHostName(redisHost);
+		jedisConnectionFactory.setPort(redisPort);
 		jedisConnectionFactory.setPoolConfig(this.getJedisPoolConfig());
-		jedisConnectionFactory.setUsePool(true);
-		jedisConnectionFactory.setTimeout(2000);
+		jedisConnectionFactory.setUsePool(usePool);
+		jedisConnectionFactory.setTimeout(redisTimeout.intValue());
 		jedisConnectionFactory.afterPropertiesSet();
 		
 		return jedisConnectionFactory;
@@ -27,10 +50,10 @@ public class JedisFactoryConfiguration {
 		
 		JedisPoolConfig poolConfig = new JedisPoolConfig();
 		
-		poolConfig.setMaxWaitMillis(2000);
-		poolConfig.setMaxTotal(100);
-		poolConfig.setMinIdle(5);
-		poolConfig.setMaxIdle(10);
+		poolConfig.setMaxWaitMillis(redisTimeout);
+		poolConfig.setMaxTotal(redisMaxTotal);
+		poolConfig.setMinIdle(redisMinIdle);
+		poolConfig.setMaxIdle(redisMaxIdle);
 		
 		return poolConfig;
 	}
